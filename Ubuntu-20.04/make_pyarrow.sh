ee5ed221afe557328aa1c575599b60b071f0e140
Hossein AKhlaghpour <hosseinakhlaghpour@gmail.com>
2020-12-27 22:57:46 +0000

    keep it

---
+++ Ubuntu-20.04/make_pyarrow.sh
@@ -0,0 +1,38 @@
+
+#! /bin/bash
+
+# Linux dependencies
+sudo apt-get install \
+  autoconf \
+  bison \
+  clang-7 \
+  cmake \
+  curl \
+  flex \
+  libboost-dev \
+  libboost-filesystem-dev \
+  libboost-regex-dev \
+  libboost-system-dev \
+  libjemalloc-dev \
+  libssl-dev \
+  llvm-7-dev \
+  python3-dev
+
+rm -fr apache-arrow-*.zip
+wget -q https://github.com/apache/arrow/archive/apache-arrow-0.15.1.zip
+rm -fr arrow-apache-arrow-*
+unzip -qq apache-arrow-0.15.1.zip
+cd arrow-apache-arrow-0.15.1/cpp
+mkdir release
+cd release
+export ARROW_HOME=/usr/local/lib
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+sudo touch /usr/lib/llvm-9/bin/yaml-bench
+cmake -DCMAKE_INSTALL_PREFIX=$ARROW_HOME -DCMAKE_INSTALL_LIBDIR=lib  -DARROW_FLIGHT=ON -DARROW_GANDIVA=ON \
+ -DARROW_ORC=ON  -DARROW_WITH_BZ2=ON -DARROW_WITH_ZLIB=ON -DARROW_WITH_ZSTD=ON  -DARROW_WITH_LZ4=ON \
+ -DARROW_WITH_SNAPPY=ON  -DARROW_WITH_BROTLI=ON -DARROW_PARQUET=ON  -DARROW_PYTHON=ON -DARROW_PLASMA=ON  \
+ -DARROW_CUDA=ON  -DARROW_BUILD_TESTS=ON -DPYTHON_EXECUTABLE=/usr/bin/python3  -DARROW_INCLUDE_DIR=src/arrow \
+ -DARROW_FULL_SO_VERSION=100.0.0 -DARROW_SO_VERSION=100.0.0 ..
+make --jobs=`nproc`
+sudo make install
